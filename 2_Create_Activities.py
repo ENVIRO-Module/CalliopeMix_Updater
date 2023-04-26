19851064e94d93342ad674aecdccc4a2ea0f471a
Alexander <altz7@hotmail.com>
2023-04-26 11:45:53 +0200

    Update

--- 2_Create_Activities.py
+++ 2_Create_Activities.py
@@ -27,6 +27,36 @@ except AssertionError:
 df=pd.read_csv('data.csv',delimiter=';')
 
 
+for index,row in df.iterrows():
+    print(row['Product'])
+    #Check if we're creating a new activity
+
+    if row['Activity']=='Yes':
+        activity_name = str(row['Activity name'])
+        print(activity_name)
+        try:
+            new_activity=ei_copy.new_activity(name=str(row['Activity name']),code=str(row['Code']))
+            new_activity.save()
+        #create a df containing the rows
+
+        except bd.errors.DuplicateNode:
+            new_activity=ei_copy.get(code=str(row['Code']))
+            new_activity.delete()
+            new_activity = ei_copy.new_activity(name=str(row['Activity name']), code=str(row['Code']))
+            new_activity.save()
+
+        act_df=df.loc[df['Activity_origin']==activity_name]
+        for ind,row2 in act_df.iterrows():
+            act=bd.Database('CUTOFF').get(code=row2['Product'])
+            exchange=new_activity.new_exchange(input=act,type='technosphere',amount=row2['Amount'])
+            exchange.save()
+        #check it worked:
+
+        print(list(new_activity.technosphere()))
+    else:
+        pass
+
+
 pass
 
 
