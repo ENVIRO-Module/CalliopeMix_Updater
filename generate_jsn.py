2b692c458870730bdef10251ee3bbd4d0cb73e3e
Alexander <altz7@hotmail.com>
2023-07-24 13:27:31 +0200

    Include some results to check

--- generate_jsn.py
+++
@@ -1,9 +0,0 @@
-import json
-
-paths={
-    'basefile': 'C:/Users/altz7/UAB/LIVENlab - SEEDS - _Alex/Hydrogen/base_file_simplified.xlsx'
-}
-with open('general.json', mode='w') as file:
-    json.dump(paths, file, indent=3)
-
-
